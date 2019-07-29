cef0980637fc389be183d902873ba33f40198302
David H Hagan <david@davidhhagan.com>
2019-07-29 08:59:12 -0400

    try re-pushing

--- docs/_build/html/generated/opcsim-plots-histplot-1.py
+++
@@ -1,7 +0,0 @@
-import opcsim, seaborn as sns
-opc = opcsim.OPC(wl=0.658, n_bins=10, dmin=0.3)
-opc.calibrate("psl")
-d = opcsim.load_distribution("Urban")
-ax = opcsim.plots.histplot(opc.evaluate(d), opc.bins)
-ax.set_ylabel("$dN/dlogD_p$")
-sns.despine()
