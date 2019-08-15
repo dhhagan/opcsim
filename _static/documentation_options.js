673b9c72275ce1613283960ed47b3aae69b66614
David H Hagan <david@davidhhagan.com>
2019-08-15 18:16:42 -0400

    publish new docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.0+31.g1c5f759.dirty',
+    VERSION: 'v0.3-rc1+3.g7a757dd.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
