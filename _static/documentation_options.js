327a68550d8dfc5c6f75ccde86ca59f821e1adda
David H Hagan <david@davidhhagan.com>
2020-05-14 15:20:28 -0400

    publish new docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,10 +1,10 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.4.0+4.g1330c40.dirty',
+    VERSION: 'v0.4.1+0.g14bd507.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false,
+    NAVIGATION_WITH_KEYS: false
 };
