c66481097bb41c9a4b5baf2a45107839c66af2ad
David H Hagan <david@davidhhagan.com>
2020-05-25 15:40:07 -0400

    retry building docs

--- docs/_build/html/_static/documentation_options.js
+++ docs/_build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.4.1+0.g14bd507.dirty',
+    VERSION: '0.5.0rc5',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
